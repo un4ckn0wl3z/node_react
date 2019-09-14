148c7742fbfe1ff41cae43e99d0a8351776b47a6
un4ckn0wl3z <slickerth@gmail.com>
2019-09-14 16:47:04 +0700

    add user validate

--- nodeapi/routes/auth.js
+++ nodeapi/routes/auth.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { signup } = require('../controllers/auth');
 const router = express.Router();
+const validator = require('../validator');
 
-router.post('/signup', signup);
+router.post('/signup', validator.userSignupValidator, signup);
 
 
 module.exports = router;
--- nodeapi/validator/index.js
+++ nodeapi/validator/index.js
@@ -9,3 +9,14 @@ exports.createPostValidator = (req, res, next) => {
     if (error) return res.status(400).send({ error: error.details[0].message });
     next();
 }
+
+exports.userSignupValidator = (req, res, next) => {
+    const schema = {
+        name: Joi.string().min(4).max(32).required(),
+        email: Joi.string().min(4).max(32).required().email(),
+        password: Joi.string().min(6).max(15).required(),
+    };
+    const { error } = Joi.validate(req.body, schema);
+    if (error) return res.status(400).send({ error: error.details[0].message });
+    next();
+}
