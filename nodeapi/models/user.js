c60b1df0d83500c2ef8083ad93b8bd1174f161a1
un4ckn0wl3z <slickerth@gmail.com>
2019-09-14 16:13:05 +0700

    implement cryptography for encrypt password

---
+++ nodeapi/models/user.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const uuidv1 = require('uuid/v1');
+const crypto = require('crypto');
+
+const userSchema = mongoose.Schema({
+    name: { type: String, trim: true, required: true },
+    email: { type: String, trim: true, required: true },
+    hashed_password: { type: String, required: true },
+    salt: String,
+    created: { type: Date, default: Date.now },
+    updated: Date
+});
+
+// virtual field
+userSchema.virtual('password')
+    .set(function (password) {
+        // create tmp var called _password
+        this._password = password;
+        // generate a timestamp
+        this.salt = uuidv1();
+        // encrypt
+        this.hashed_password = this.encryptPassword(password);
+    })
+    .get(function () {
+        return this._password;
+    })
+
+// methods
+userSchema.methods = {
+    encryptPassword: function (password) {
+        if (!password) return "";
+        try {
+            return crypto.createHmac('sha1', this.salt)
+                .update(password)
+                .digest('hex');
+        } catch (error) {
+            return "";
+        }
+
+    }
+}
+
+module.exports = mongoose.model("User", userSchema);
