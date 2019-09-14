df2c34fd7d0f4a1b5a58f2f2821f00de298f2461
un4ckn0wl3z <slickerth@gmail.com>
2019-09-14 11:14:47 +0700

    install express

--- node-basic/app.js
+++ node-basic/app.js
@@ -2,6 +2,12 @@
 const { sum, multiply } = require('./helpers');
 const http = require('http');
 
+const server = http.createServer((req, res) => {
+    res.end("Hello World From NodeJS...");
+});
+
+server.listen(3000);
+
 // var let const
 const total = sum(5, 3);
 const mul = multiply(5, 3);
