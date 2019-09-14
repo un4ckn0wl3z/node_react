77e4358ebd064feab0f20eb0186853a71b6bc656
un4ckn0wl3z <slickerth@gmail.com>
2019-09-14 21:31:22 +0700

    update create post method with can uplaod image and referrenc to user owner of the post

--- nodeapi/controllers/post.js
+++ nodeapi/controllers/post.js
@@ -1,26 +1,46 @@
 const Post = require('../models/post');
+const formidable = require('formidable');
+const fs = require('fs');
+
 
 exports.getPosts = (req, res) => {
     const posts = Post.find().select('_id title body')
-    .then(posts => {
-        res.json({
-            posts
+        .then(posts => {
+            res.json({
+                posts
+            });
+        }).catch(err => {
+            res.status(400).json({
+                error: err
+            });
         });
-    }).catch(err => {
-        res.status(400).json({
-            error: err
-        });
-    });
 }
 
-exports.createPost = (req, res) => {
-    const post = new Post(req.body);
-    post.save().then(result => {
-        res.json({
-            post: result
+exports.createPost = (req, res, next) => {
+
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            return res.status(400).json({
+                error: 'Image could not be uploaded.'
+            });
+        }
+        let post = new Post(fields);
+        post.postedBy = req.profile;
+        if (files.photo) {
+            post.photo.data = fs.readFileSync(files.photo.path);
+            post.photo.contentType = files.photo.type;
+        }
+        post.save((err, result) => {
+            if (err) {
+                return res.status(400).json({
+                    error: err
+                });
+            }
+            res.status(400).json(result);
         });
+
     });
-
-
 }
 
--- nodeapi/models/post.js
+++ nodeapi/models/post.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Schema;
 
 const postSchema = mongoose.Schema({
     title: { type: String, required: true },
-    body: { type: String, required: true }
+    body: { type: String, required: true },
+    photo: { type: Buffer, contentType: String },
+    postedBy: { type: ObjectId, ref: 'User' },
+    created: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model("Post", postSchema);
