f61dbd1bf9fb617413381f9feaf6c489b47a1aa3
un4ckn0wl3z <slickerth@gmail.com>
2019-09-14 12:59:20 +0700

    adding post route

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello World from NodeJS.");
-});
+/** bring routes here */
+
+const { getPosts } = require('./routes/post');
+
+app.get("/", getPosts);
 
 const port = 8080;
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`A NodeJS API listening on port: ${port}`);
 });
---
+++ nodeapi/routes/post.js
@@ -0,0 +1,6 @@
+
+ exports.getPosts = (req, res) => {
+    res.send("Hello World from NodeJS.");
+}
+
+
