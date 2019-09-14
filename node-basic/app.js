d649ac6e79048d556e9d3a206208044012bc383a
un4ckn0wl3z <slickerth@gmail.com>
2019-09-14 11:44:54 +0700

    sync and async

--- node-basic/app.js
+++ node-basic/app.js
@@ -1,8 +1,27 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello From Express....');
-});
+// app.get('/', (req, res) => {
+//     res.send('Hello From Express....');
+// });
 
-app.listen(3000);
+// app.listen(3000);
+
+const fs = require('fs')
+const fileName = "target.txt";
+
+// fs.watch(fileName, () => {
+//     console.log('File changed!');
+// });
+
+// fs.readFile(fileName, (err, data) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(data.toString());
+
+// });
+
+const data = fs.readFileSync(fileName);
+console.log(data.toString());
+console.log('NodeJS Async Program.');
