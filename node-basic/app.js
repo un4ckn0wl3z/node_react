705dd08c5ef1d926389fc65bc3faab13e4364f06
un4ckn0wl3z <slickerth@gmail.com>
2019-09-14 10:37:46 +0700

    init

---
+++ node-basic/app.js
@@ -0,0 +1,10 @@
+// console.log("PROCESS: ", process);
+
+const helpers = require('./helpers');
+
+// var let const
+const total = helpers.sum(5,3);
+const mul = helpers.multiply(5,3);
+
+console.log('Total: ' + total);
+console.log('Mul: ' + mul);
