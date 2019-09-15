e51f78a94c504178e06e741ba0e7fd07356ac8cd
un4ckn0wl3z <slickerth@gmail.com>
2019-09-15 13:24:33 +0700

    setup project with router

--- react-front/src/App.js
+++ react-front/src/App.js
@@ -1,18 +1,11 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import MainRouter from './MainRouter';
 
-class App extends Component {
-  constructor(props){
-    super(props);
-  }
-
-  render(){
-    return(
-      <div className="container" >
-        <h1>React Frontend</h1>
-      </div>
-    );
-  }
-
-}
+const App = () => (
+  <BrowserRouter>
+    <MainRouter/>
+  </BrowserRouter>
+)
 
 export default App;
