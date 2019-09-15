a15894df965e4263c5d4de3b419258be97d5dab9
un4ckn0wl3z <slickerth@gmail.com>
2019-09-15 13:05:49 +0700

    start new frontend project

---
+++ react-front/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <div className="container" >
+        <h1>React Frontend</h1>
+      </div>
+    );
+  }
+
+}
+
+export default App;
