868e336b34d34da1d44a52986c39e3a573d938b8
un4ckn0wl3z <slickerth@gmail.com>
2019-09-15 11:36:12 +0700

    conditional rendering

--- react-first-time/my-app/src/App.js
+++ react-first-time/my-app/src/App.js
@@ -7,23 +7,37 @@ class App extends Component {
     // state
 
     this.state = {
-      users: []
+      users: [],
+      loading: false
     }
+  }
 
+  getUsers(){
+    this.setState({
+      loading: true
+    });
+    axios('https://api.randomuser.me/?nat=US&results=5').then(res => {
+      this.setState({
+        users: res.data.results,
+        loading: false
+      });
+    }).catch();
   }
 
   componentWillMount() {
-    axios('https://api.randomuser.me/?nat=US&results=5').then(res => {
-      this.setState({
-        users: res.data.results
-      });
-    }).catch();
+    this.getUsers();
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.users.map(user => <div><h3>{user.name.first}</h3></div>)}
+        { !this.state.loading ? this.state.users.map(user => 
+        <div>
+          <h3>{user.name.first}</h3>
+          <br />
+          <p>{user.email}</p>
+          <hr/>
+        </div>) : 'Loading....'}
       </div>
     );
   }
