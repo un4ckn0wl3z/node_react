53a825760eeeb7b3ddd32338721226ad72819e60
un4ckn0wl3z <slickerth@gmail.com>
2019-09-15 15:28:22 +0700

    refactor signup form

--- react-front/src/user/Signup.js
+++ react-front/src/user/Signup.js
@@ -7,27 +7,25 @@ class Signup extends Component {
             name: "",
             email: "",
             password: "",
-            error: ""
+            error: "",
+            open: false
         }
     }
 
     handleChange = (name) => (event) => {
         this.setState({
+            error: "",
+            open: false
+        });
+        this.setState({
             [name]: event.target.value
         });
     }
 
-    clieckSubmit = event => {
-        event.preventDefault();
-        const { name, email, password } = this.state;
-        const user = {
-            name,
-            email,
-            password
-        }
-        fetch("http://localhost:8080/auth/signup",{
+    signup = user => {
+        return fetch("http://localhost:8080/auth/signup", {
             method: 'POST',
-            headers:{
+            headers: {
                 Accept: "application/json",
                 "Content-Type": "application/json",
             },
@@ -39,26 +37,62 @@ class Signup extends Component {
         });
     }
 
-    render() {
+
+    clieckSubmit = event => {
+        event.preventDefault();
         const { name, email, password } = this.state;
+        const user = {
+            name,
+            email,
+            password
+        }
+
+        this.signup(user).then(data => {
+            if (data.error) {
+                this.setState({
+                    error: data.error
+                });
+            } else {
+                this.setState({
+                    name: "",
+                    email: "",
+                    password: "",
+                    error: "",
+                    open: true
+                });
+            }
+        });
+
+    }
+
+    signupForm = (name, email, password) => (
+        <form>
+            <div className="form-group">
+                <label className="text-muted">Name</label>
+                <input onChange={this.handleChange("name")} type="text" className="form-control" value={name} />
+            </div>
+            <div className="form-group">
+                <label className="text-muted">email</label>
+                <input onChange={this.handleChange("email")} type="email" className="form-control" value={email} />
+            </div>
+            <div className="form-group">
+                <label className="text-muted">Password</label>
+                <input onChange={this.handleChange("password")} type="password" className="form-control" value={password} />
+            </div>
+            <button onClick={this.clieckSubmit} className="btn btn-primary btn-raised">Submit</button>
+        </form>
+    )
+
+    render() {
+        const { name, email, password, error, open } = this.state;
         return (
             <div className="container" >
                 <h2 className="mt-5 mb-5" >Signup</h2>
-                <form>
-                    <div className="form-group">
-                        <label className="text-muted">Name</label>
-                        <input onChange={this.handleChange("name")} type="text" className="form-control"  value={name}/>
-                    </div>
-                    <div className="form-group">
-                        <label className="text-muted">email</label>
-                        <input onChange={this.handleChange("email")} type="email" className="form-control"  value={email} />
-                    </div>
-                    <div className="form-group">
-                        <label className="text-muted">Password</label>
-                        <input onChange={this.handleChange("password")} type="password" className="form-control" value={password} />
-                    </div>
-                    <button onClick={this.clieckSubmit} className="btn btn-primary btn-raised">Submit</button>
-                </form>
+
+                <div className="alert alert-danger" style={{ display: error ? "" : 'none' }} >{error}</div>
+                <div className="alert alert-info" style={{ display: open ? "" : 'none' }} >
+                    New account is successfully created. Please sign In.</div>
+                {this.signupForm(name, email, password)}
             </div>
         );
     }
