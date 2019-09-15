8489f33a51b065b05cdd53349d36ae808e6f4669
un4ckn0wl3z <slickerth@gmail.com>
2019-09-15 11:22:39 +0700

    play with state

--- react-first-time/my-app/src/App.js
+++ react-first-time/my-app/src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      We will be back!
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    // state
+
+    this.state = {
+      users: []
+    }
+
+  }
+
+  componentWillMount() {
+    axios('https://api.randomuser.me/?nat=US&results=5').then(res => {
+      this.setState({
+        users: res.data.results
+      });
+    }).catch();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.users.map(user => <div><h3>{user.name.first}</h3></div>)}
+      </div>
+    );
+  }
 }
 
 export default App;
