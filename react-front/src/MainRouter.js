c3787a9eac90dd27394846e296421fd1701cfa7a
un4ckn0wl3z <slickerth@gmail.com>
2019-09-15 14:45:22 +0700

    test signup

--- react-front/src/MainRouter.js
+++ react-front/src/MainRouter.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './core/Home';
+import Signup from './user/Signup';
+
 
 const MainRouter = () => (
     <div>
         <Switch>
-            <Route path="/" component={Home} ></Route>
+            <Route exact path="/" component={Home} ></Route>
+            <Route exact path="/signup" component={Signup} ></Route>
         </Switch>
     </div>
 )
