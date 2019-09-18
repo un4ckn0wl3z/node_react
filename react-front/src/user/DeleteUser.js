300b53da97f79eadda140e2c092d0f70433b50ea
un4ckn0wl3z <slickerth@gmail.com>
2019-09-18 21:59:19 +0700

    - update profile page with props - extract delete button to another component

---
+++ react-front/src/user/DeleteUser.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+class DeleteUser extends Component {
+    render() {
+        return (
+            <button className="btn btn-raised btn-danger mr-5">Delete Profile</button>
+        );
+    }
+}
+
+export default DeleteUser;
--- react-front/src/user/Profile.js
+++ react-front/src/user/Profile.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { isAuthenticated } from '../auth';
 import { Redirect, Link } from 'react-router-dom';
 import { read } from './apiUser';
+import defaultProfileImg from '../img/default-user.png';
+import DeleteUser from './DeleteUser';
 
 class Profile extends Component {
     constructor() {
@@ -41,31 +43,39 @@ class Profile extends Component {
 
     }
 
+    componentWillReceiveProps(props) {
+        const userId = props.match.params.userId;
+        this.init(userId);
+
+    }
+
     render() {
-        const {redirectToSignin, user} = this.state;
+        const { redirectToSignin, user } = this.state;
         if (redirectToSignin) return <Redirect to="/signin" />
 
         return (
             <div className="container">
+                 <h2 className="mt-5 mb-5" >Profile</h2>
                 <div className="row">
                     <div className="col-md-6">
-                        <h2 className="mt-5 mb-5" >Profile</h2>
-                        <p>Hello {user.name}</p>
-                        <p>Email: {user.email}</p>
-                        <p>{`Joined: ${new Date(user.created).toDateString()}`}</p>
+                        <img className="card-img-top" src={defaultProfileImg} alt={user.name} style={{ width: '100%', height: '15vw', objectFit: 'cover' }} />
                     </div>
                     <div className="col-md-6">
+                        <div className="lead mt-2">
+                            <p>Hello {user.name}</p>
+                            <p>Email: {user.email}</p>
+                            <p>{`Joined: ${new Date(user.created).toDateString()}`}</p>
+                        </div>
                         {
                             isAuthenticated().user && isAuthenticated().user._id == user._id
                             && (
                                 <div className="d-inline-black mt-5">
                                     <Link to={`/user/edit/${user._id}`} className="btn btn-raised btn-success mr-5">Edit Profile</Link>
-                                    <button className="btn btn-raised btn-danger mr-5">Delete Profile</button>
-
+                                    <DeleteUser />
                                 </div>
                             )
                         }
-                </div>
+                    </div>
                 </div>
             </div>
         );
